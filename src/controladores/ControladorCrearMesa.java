9c89708eb92fa5d5ea6a1939f3d15206429f5048
tomas <tomasmartynowicz@gmail.com>
2017-06-05 11:59:14 -0300

    nuevo controlador para crear mesa

---
+++ src/controladores/ControladorCrearMesa.java
@@ -0,0 +1,55 @@
+package controladores;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import negocio.MesaABM;
+
+public class ControladorCrearMesa extends HttpServlet {
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) 
+	 throws ServletException, IOException {
+		procesarPeticion(request, response);
+	}
+	
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) 
+	 throws ServletException, IOException {
+		procesarPeticion(request, response);
+	}
+
+	private void procesarPeticion(HttpServletRequest request, HttpServletResponse response) 
+			 throws ServletException, IOException {
+		
+			try{				
+				response.setContentType("text/html;charset=UTF-8");
+				int salon = Integer.parseInt(request.getParameter("salon"));
+				int nroMesa = Integer.parseInt(request.getParameter("nroMesa"));
+				int cantidadComensales = Integer.parseInt(request.getParameter("cantidadComensales"));
+				MesaABM abm = new MesaABM();
+				
+				abm.crearMesa(cantidadComensales, salon, nroMesa);				
+				
+				String mensaje = "Mesa creada";
+				
+				request.setAttribute("mensaje", mensaje);
+				request.getRequestDispatcher("/ajaxopfinalizada.jsp").forward(request, response);
+				
+			} catch(Exception e) {
+				String mensaje = "Error: la mesa ya existe en la base de datos. Ingrese una nueva mesa";
+				
+				request.setAttribute("mensaje", mensaje);
+				request.getRequestDispatcher("/ajaxopfinalizada.jsp").forward(request, response);
+			}
+				
+				
+				
+				
+	} 
+}
